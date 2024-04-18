a48dd36a388999645e7ba02449fe3f04df3087ef
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-04-18 16:04:26 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,16 @@
 import pandas as pd
-#import psycopg2
-#import psycopg2.extras
-#import sqlalchemy
 import streamlit as st
-#import pydoc    
 import pyodbc
 
-def get_connection():
-    # Connect to Database
-    #engine = sqlalchemy.create_engine(
-    #    "postgresql+psycopg2://reader:NWDMCE5xdipIjRrp@hh-pgsql-public.ebi.ac.uk/pfmegrnargs")
-
+#def get_connection():
+    # Connect to Database 
+con=pyodbc.connect(driver='postgresql',
+           database='pfmegrnargs',
+           uid='reader',
+           pwd='NWDMCE5xdipIjRrp',
+           server='hh-pgsql-public.ebi.ac.uk',
+           port=5432)
     
-    con=pyodbc.connect(driver='postgresql',
-                   database='pfmegrnargs',
-                   uid='reader',
-                   pwd='NWDMCE5xdipIjRrp',
-                   server='hh-pgsql-public.ebi.ac.uk',
-                   port=5432)
-    
-    #PWD = Sys.getenv("CONNECTION_RNA_CENTRAL_PASSWORD"), BoolsAsChar = "NWDMCE5xdipIjRrp", 
-    #timeout = 10)
 
     # retrieve a table from the database databases
     query = "SELECT * FROM auth_permission"
