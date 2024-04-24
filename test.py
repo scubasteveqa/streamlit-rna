42b40d667e298620892df2a9418cd640f0aeb77e
scubasteveqa <154364741+scubasteveqa@users.noreply.github.com>
2024-04-24 10:34:47 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pyodbc
 
 # Connect to Database 
-con=pyodbc.connect(driver='postgresql',
+con=pyodbc.connect(driver='PostgreSQL ODBC Driver',
            database='pfmegrnargs',
            uid='reader',
            pwd='NWDMCE5xdipIjRrp',
